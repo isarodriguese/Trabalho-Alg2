a2f4f76b214ebd022e939337e106f6ddb3250c9d
Isadora Rodrigues <isadorarodrigues@ufpr.br>
2025-11-06 15:25:09 -0300

    ainda não está 100% pronto, mas so pra ja salvar

---
+++ codigosC/functions.h
@@ -0,0 +1,29 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+struct paciente;
+
+struct paciente *InicHeap ();
+int InsereHeap ();
+int RemoveHeap ();
+void Heapfy ();
+int ChecaHeap ();
+void ImprimeHeap ();
+void HeapSort ();
+void AlteraHeap ();
+void SacodeHeap ();
+void ImprimeMenu ();
+
+void QuickSort();
+void SelectSort();
+void GeraVetor();
+int *CopiaVetor();
+void ImprimeVetor();
+void SacodeHeapV();
+void HeapSortV ();
+int ChecaHeapV();
+int *InicHeapV();
+int InsereHeapV();
+void HeapfyV();
+
+#endif
